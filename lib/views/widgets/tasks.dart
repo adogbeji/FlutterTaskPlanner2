56b121cfad9337bd9125881b82f71ef9714cc543
adogbeji <baka_1993@yahoo.co.uk>
2024-09-16 14:12:31 +0100

    Implement MVC structure, add starting code for Tasks widget & remove default app code

---
+++ lib/views/widgets/tasks.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Tasks extends StatefulWidget {
+  const Tasks({super.key});
+
+  @override
+  State<Tasks> createState() => _TasksState();
+}
+
+class _TasksState extends State<Tasks> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Tasks Widget'),
+      ),
+    );
+  }
+}
