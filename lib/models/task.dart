6c2c0aa70a6391ed9af373dc4bbcb5896d25d884
adogbeji <baka_1993@yahoo.co.uk>
2024-09-19 13:50:40 +0100

    Start using intl & uuid packages in Task model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,10 +1,21 @@
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+final formatter = DateFormat.yMMMd();
+
 enum Category { food, leisure, work, travel }
 
 class Task {
-  Task({required this.title, required this.date, required this.category});
+  Task({required this.title, required this.date, required this.category}): id = uuid.v4();
 
   final String id;
   final String title;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
