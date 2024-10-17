14de6342f9d073b6a27a46c6f0ecdde64d8795b9
adogbeji <baka_1993@yahoo.co.uk>
2024-10-17 13:50:38 +0100

    Write code to add new task & close dialog in NewTask widget

--- lib/views/widgets/new_task.dart
+++ lib/views/widgets/new_task.dart
@@ -60,6 +60,16 @@ class _NewTaskState extends State<NewTask> {
       );
       return;  // Stops function execution
     }
+
+    widget.onAddTask(
+      Task(
+        title: _nameController.text,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
+
+    Navigator.pop(context);  // Closes dialog
   }
 
   @override
